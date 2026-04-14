d18775b5b83f21be7a8271e8aa74c0ffa950f75a
Winston Hsiao <hsiaowinston@gmail.com>
2026-04-14 19:09:05 -0400

    feat: one time migration for raypaste data to rayvise

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import "#/App.css";
 import { Layout } from "#/components/Layout";
 import { useApplyDocumentTheme } from "#/hooks/useApplyDocumentTheme";
+import { useLegacyStateMigration } from "#/hooks/useLegacyStateMigration";
 
 function App() {
+  useLegacyStateMigration();
   useApplyDocumentTheme();
 
   return <Layout />;
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { RenderWindow } from "#/components/RenderWindow";
+import { runLegacyMigrations } from "#/lib/legacyMigration";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement,
-);
+async function bootstrap() {
+  await runLegacyMigrations();
 
-root.render(
-  <React.StrictMode>
-    <RenderWindow />
-  </React.StrictMode>,
-);
+  const { RenderWindow } = await import("#/components/RenderWindow");
+  const root = ReactDOM.createRoot(
+    document.getElementById("root") as HTMLElement,
+  );
+
+  root.render(
+    <React.StrictMode>
+      <RenderWindow />
+    </React.StrictMode>,
+  );
+}
+
+void bootstrap();
