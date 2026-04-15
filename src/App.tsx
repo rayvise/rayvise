2acc2fa7d9f9e75504ffe3350049e2fabbb9e062
Winston Hsiao <hsiaowinston@gmail.com>
2026-04-14 20:12:26 -0400

    Revert "feat: one time migration for raypaste data to rayvise"
    This reverts commit d18775b5b83f21be7a8271e8aa74c0ffa950f75a.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import "#/App.css";
 import { Layout } from "#/components/Layout";
 import { useApplyDocumentTheme } from "#/hooks/useApplyDocumentTheme";
-import { useLegacyStateMigration } from "#/hooks/useLegacyStateMigration";
 
 function App() {
-  useLegacyStateMigration();
   useApplyDocumentTheme();
 
   return <Layout />;
--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { runLegacyMigrations } from "#/lib/legacyMigration";
+import { RenderWindow } from "#/components/RenderWindow";
 
-async function bootstrap() {
-  await runLegacyMigrations();
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement,
+);
 
-  const { RenderWindow } = await import("#/components/RenderWindow");
-  const root = ReactDOM.createRoot(
-    document.getElementById("root") as HTMLElement,
-  );
-
-  root.render(
-    <React.StrictMode>
-      <RenderWindow />
-    </React.StrictMode>,
-  );
-}
-
-void bootstrap();
+root.render(
+  <React.StrictMode>
+    <RenderWindow />
+  </React.StrictMode>,
+);
